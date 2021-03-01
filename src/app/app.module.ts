c747bafacec08a06948a7851c74ec185ecebbfb9
Pankaj <Pankaj@DESKTOP-BFUD1MF>
2021-03-01 15:27:29 +0530

    InitialCommit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EvenComponent } from './even/even.component';
+import { OddComponent } from './odd/odd.component';
+import { GameControlComponent } from './game-control/game-control.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EvenComponent,
+    OddComponent,
+    GameControlComponent
   ],
   imports: [
     BrowserModule,
