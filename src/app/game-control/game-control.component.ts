d9da197902c9d16c689305c56283d43c6068211b
Pankaj <Pankaj@DESKTOP-BFUD1MF>
2021-03-01 17:08:26 +0530

    Commit Functionality

--- src/app/game-control/game-control.component.ts
+++ src/app/game-control/game-control.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-game-control',
   templateUrl: './game-control.component.html',
-  styleUrls: ['./game-control.component.css']
+  styleUrls: ['./game-control.component.css'],
 })
 export class GameControlComponent implements OnInit {
-
-  constructor() { }
+  @Output() firedInterval = new EventEmitter<number>();
+  interval;
+  lastNumber = 0;
+  constructor() {}
 
   ngOnInit(): void {
   }
 
+  onStart() {
+    this.interval = setInterval(() => {
+      this.firedInterval.emit(this.lastNumber + 1);
+      this.lastNumber++;
+    }, 1000);
+  }
+
+  onPause(){
+    clearInterval(this.interval);
+  }
+
+  resetGame(){
+    
+  }
 }
