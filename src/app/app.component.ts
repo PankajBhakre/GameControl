594f80bc3ca4a186bb8fa774faad752d81dfbe17
Pankaj <Pankaj@DESKTOP-BFUD1MF>
2021-03-07 17:53:12 +0530

    commit new changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  oddNumbers: number[] =[];
+  oddNumbers: number[] = [];
   evenNumbers:number[] = [];
 
   
